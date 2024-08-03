95daa0be6d2ad1aacb2aa558b2e234d8dfee6715
Sahil Dinanath <sahildinanath@gmail.com>
2024-08-03 18:45:10 +0200

    implemented output to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::{thread::sleep, time::Duration, usize};
+use std::{io::stdout, io::Result, thread::sleep, time::Duration, usize};
 
 use rand::Rng;
-fn main() {
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, KeyCode, KeyEventKind},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    style::Stylize,
+    widgets::{
+        canvas::{Canvas, Points},
+        Block,
+    },
+    Terminal,
+};
+fn main() -> Result<()> {
     //settings
-    const SIZE: usize = 20;
-    let background = ' ';
-    let cell = '■';
-    let update_speed = Duration::from_millis(200);
-    let spawn_chance = 0.09;
+    const SIZE: usize = 500;
+    let background = 0;
+    let cell = 1;
+    let update_speed = Duration::from_millis(25);
+    let spawn_chance = 0.1;
+    let cell_color = ratatui::style::Color::White;
 
     let mut matrix = [[background; SIZE]; SIZE];
 
@@ -32,6 +47,12 @@ fn main() {
         }
     }
 
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
     loop {
         for row in 0..SIZE {
             for column in 0..SIZE {
@@ -39,18 +60,18 @@ fn main() {
                 let mut counter = 0;
                 for (y, x) in moves {
                     //calculate the next move
-                    let next_row = row as i8 + y;
-                    let next_column = column as i8 + x;
+                    let next_row = row as i32 + y;
+                    let next_column = column as i32 + x;
 
                     //if next move is out of bounds, don't update
                     let row = if next_row < 0 || next_row as usize >= SIZE {
-                        row
+                        continue;
                     } else {
                         next_row as usize
                     };
 
                     let column = if next_column < 0 || next_column as usize >= SIZE {
-                        column
+                        continue;
                     } else {
                         next_column as usize
                     };
@@ -68,10 +89,55 @@ fn main() {
                 }
             }
         }
-        for row in matrix {
-            println!("{:?}", row);
+        // for row in matrix {
+        //     println!("{:?}", row);
+        // }
+        // println!("");
+
+        terminal.draw(|frame| {
+            let area = frame.size();
+            let canvas = Canvas::default()
+                .block(Block::default())
+                .paint(|ctx| {
+                    let mut points: [(f64, f64); SIZE * SIZE] = [(0.0, 0.0); SIZE * SIZE];
+                    let mut counter = 0;
+                    for row in 0..SIZE {
+                        for column in 0..SIZE {
+                            if matrix[row][column] == cell {
+                                points[counter] = (column as f64, row as f64);
+                                counter += 1;
+                                // ctx.draw(&Rectangle {
+                                //     x: column as f64,
+                                //     y: row as f64,
+                                //     width: 0.1,
+                                //     height: 0.1,
+                                //     color: ratatui::style::Color::White,
+                                // })
+                            }
+                        }
+                    }
+                    ctx.draw(&Points {
+                        coords: &points,
+                        color: cell_color,
+                    })
+                })
+                .x_bounds([0.0, SIZE as f64])
+                .y_bounds([0.0, SIZE as f64]);
+
+            frame.render_widget(canvas, area);
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
         }
-        println!("");
         sleep(update_speed);
     }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
